4fd1fa13ab7bf31ef4d5f8992cd9b1c41faa4d62
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-06-16 02:53:21 +0530

    feat: added users and bookmarks table to database

--- services/db-service/initdb/01_create_tables.sql
+++ services/db-service/initdb/01_create_tables.sql
@@ -10,20 +10,22 @@ CREATE TABLE
     websiteUrl VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT NOW (),
     updated_at TIMESTAMP DEFAULT NOW ()
+  );
+
+CREATE TABLE
+  users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    profile_picture TEXT,
+    created_at TIMESTAMP DEFAULT NOW ()
+  );
+
+CREATE TABLE
+  bookmarks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    company_id UUID NOT NULL REFERENCES companies (id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT NOW (),
+    UNIQUE (user_id, company_id)
   )
-  -- CREATE TABLE jobs(
-  --   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  --   title VARCHAR(500) NOT NULL,
-  --   location VARCHAR(255) NOT NULL,
-  --   description TEXT NOT NULL,
-  --   salary INTEGER,
-  --   category VARCHAR(100) NOT NULL,
-  --   company VARCHAR(255) NOT NULL,
-  --   companyWebsite VARCHAR(255) NOT NULL,
-  --   companyLogo VARCHAR(255),
-  --   jobUrl VARCHAR(255) NOT NULL UNIQUE,
-  --   postedAt DATE,
-  --   scrapedAt TIMESTAMP DEFAULT NOW(),
-  --   created_at TIMESTAMP DEFAULT NOW(),
-  --   updated_at TIMESTAMP DEFAULT NOW()
-  -- );
--- services/db-service/initdb/02_create_indexes.sql
+++ services/db-service/initdb/02_create_indexes.sql
@@ -1,10 +1,3 @@
--- CREATE INDEX idx_salary ON jobs(salary);
--- CREATE INDEX idx_category ON jobs(category);
--- CREATE INDEX idx_company ON jobs(company);
--- CREATE INDEX idx_location ON jobs(location);
--- CREATE INDEX idx_location_category ON jobs(location, category);
--- CREATE INDEX idx_location_salary ON jobs(location, salary);
--- CREATE INDEX idx_jobs_search ON jobs USING GIN(to_tsvector('english', title || ' ' || description));
 CREATE INDEX idx_companies_location ON companies (location);
 
 CREATE INDEX idx_companies_name ON companies (companyName);
